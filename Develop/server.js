08283e9413178ba7f4cfd654989522bff4c60019
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2022-03-29 17:00:31 -0400

    server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 
+const seedCategories = require('./seeds/category-seeds')
+const seedProducts = require('./seeds/product-seeds')
+const seedProductTags = require('./seeds/product-tag-seeds')
+const seedTags = require('./seeds/tag-seeds')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: true }).then(() => {
+
+  seedCategories();
+  seedProducts();
+  seedProductTags();
+  seedTags();
+}).then(() => {
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+})
